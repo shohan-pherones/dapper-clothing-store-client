d1baeed83ff0eba99295d1cd780c08387746d9fd
shohan-pherones <shohan.sub.56@gmail.com>
2022-12-04 22:13:44 +0600

    featured added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from "../../components/Featured/Featured";
 import Slider from "../../components/Slider/Slider";
 import "./Home.scss";
 
@@ -5,6 +6,8 @@ const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <Featured type="featured" />
+      <Featured type="trending" />
     </div>
   );
 };
